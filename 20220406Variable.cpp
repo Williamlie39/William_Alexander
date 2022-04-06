5e68f993843cab89899c2c74983f108e4b9c4632
Williamlie39 <103086048+Williamlie39@users.noreply.github.com>
2022-04-06 11:11:18 +0800

    Create 20220406Variable.cpp

---
+++ 20220406Variable.cpp
@@ -0,0 +1,12 @@
+//header
+#include <iostream>
+using namespace std;
+int_main() {//begin of function
+    float angka;
+    string nama; // deklarasi variabel
+    nama = "william alexander"; //masukkan value kedalam variabel
+    angka =10.21;
+    cout<<nama<<endl; //menampilkan variabel
+    cout<<angka=3.14;
+
+}//end of function
